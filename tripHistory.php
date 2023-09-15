184d88f7415863a3ec31fc22f026460a72b8ecd6
nikhilserialcom <1nikhil.serialcominfotech@gmail.com>
2023-09-15 18:17:10 +0530

    update trip history api

--- tripHistory.php
+++ tripHistory.php
@@ -6,21 +6,83 @@ header("content-type:application/json");
 if (isset($_POST['userId'])) 
 {
     $userId = $_POST['userId'];
-    $tripHistoryQuery = "SELECT * FROM book_ride WHERE userId = '$userId' or driverId = '$userId'";
-    $tripHistory = mysqli_query($con,$tripHistoryQuery);
+    $tripStatus = $_POST['tripStatus'];
 
-    if(mysqli_num_rows($tripHistory) > 0)
+    if($tripStatus == "past")
     {
-        while($row = mysqli_fetch_assoc($tripHistory))
+        $tripHistoryQuery = "SELECT * FROM completerides WHERE userId = '$userId' OR driverId = '$userId'";
+        $tripHistory = mysqli_query($con,$tripHistoryQuery);
+
+        if(mysqli_num_rows($tripHistory) > 0)
         {
-            $response['trips'][] = $row;
+            while($row = mysqli_fetch_assoc($tripHistory))
+            {
+                $response[] = $row;
+            }
+        }
+        else
+        {
+            $response['status'] = "404";
+            $response['message'] = "database empty";
         }
     }
     else
     {
-        $response['status'] = "404";
-        $response['message'] = "database empty";
+        $upcomingTripQuery = "SELECT * FROM book_ride WHERE userId = '$userId' OR driverId = '$userId'";
+        $upcomingTrip = mysqli_query($con,$upcomingTripQuery);
+
+        $time = '';
+
+        if(mysqli_num_rows($upcomingTrip) > 0)
+        {
+            while($upcoming = mysqli_fetch_assoc($upcomingTrip))
+            {
+                if($upcoming['userId'] == $userId)
+                {
+                    $dataQuery = mysqli_query($con,"SELECT driverLetitude,driverLongitude,mobile_number,Number_plate FROM user INNER JOIN vehicleinfo ON user.driverId = vehicleinfo.driverId WHERE user.driverId = '{$upcoming['driverId']}'");
+                    $data = mysqli_fetch_assoc($dataQuery);
+
+                    $driverdataQuery = "SELECT * FROM driver_request WHERE driverId = '{$upcoming['driverId']}'";
+                    $driverdata = mysqli_query($con,$driverdataQuery);
+                    $driver = mysqli_fetch_assoc($driverdata);
+
+                    $upcoming['drivername'] = $driver['firstname'];
+                    $upcoming['profile'] = $driver['photo'];
+                    $upcoming['mobile_number'] = $data['mobile_number'];
+                    $upcoming['Number_plate'] = $data['Number_plate'];
+                    $upcoming['driverLetitude'] = $data['driverLetitude'];
+                    $upcoming['driverLongitude'] = $data['driverLongitude'];
+                    $upcoming['vehicleBrand'] = $driver['vehicleBrand'];
+                    $upcoming['rating'] = $driver['rating'];
+                    $upcoming['time'] = $driver['time'];
+                    $response[] = $upcoming;
+                }
+                elseif($upcoming['driverId'] == $userId)
+                {
+                    $driverdataQuery = "SELECT * FROM driver_request WHERE driverId = '{$upcoming['driverId']}'";
+                    $driverdata = mysqli_query($con,$driverdataQuery);
+                    $driver = mysqli_fetch_assoc($driverdata);
+
+                    $dataQuery = mysqli_query($con,"SELECT * FROM user  WHERE id = '{$upcoming['userId']}'");
+                    $data = mysqli_fetch_assoc($dataQuery);
+                    $upcoming['profile'] = $data['photo'];
+                    $upcoming['mobile_number'] = $data['mobile_number'];
+                    $upcoming['time'] = $driver['time'];
+                    $response[] = $upcoming;
+                }
+                else
+                {
+                    $response['message'] = "user not found";
+                }
+            }
+        }
+        else
+        {
+            $response['staus'] = "404";
+            $response['message'] = "database empty";
+        }
     }
+    
 
 } 
 else 
